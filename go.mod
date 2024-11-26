e1e251d618af28ab5095982e2be51e46c28fd09c
Peter Lee <0.peterlee@gmail.com>
2024-11-25 17:24:33 -0800

    working

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module pepito
 
 go 1.22.1
 
-require github.com/r3labs/sse v0.0.0-20210224172625-26fe804710bc
+require github.com/r3labs/sse/v2 v2.10.0
 
 require (
 	golang.org/x/net v0.0.0-20191116160921-f9c825593386 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-  "github.com/r3labs/sse"
+  "github.com/r3labs/sse/v2"
   "encoding/json"
+  "io"
   "fmt"
   "os"
+  "net/http"
+  "bytes"
 )
 
 type configData struct {
   WebUrl string `json:"webhook_url"`
   SseUrl string `json:"sse_endpoint_url"`
   SseStream string `json:"sse_stream"`
+  AvatarUrl string `json:"webhook_avatar_url"`
+  Username string `json:"webhook_username"`
 }
 
 type streamData struct {
@@ -19,6 +24,13 @@ type streamData struct {
   Time int `json:"time"`
   Img string `json:"img"`
 }
+
+type webhookPayload struct {
+  Content   string `json:"content"`
+  Username  string `json:"username"`
+  AvatarURL string `json:"avatar_url"`
+}
+
 
 func main() {
   if len(os.Args) < 2 {
@@ -30,21 +42,39 @@ func main() {
   err := json.Unmarshal(config_bytes, &config)
   check(err, "error unmarshaling config file")
 
-  // done := make(chan bool)
-
   events := make(chan *sse.Event)
 
   sse_client := sse.NewClient(config.SseUrl)
   sse_client.SubscribeChan(config.SseStream, events)
+
+  client := &http.Client{}
 
   for event := range events {
     var data streamData
     err = json.Unmarshal(event.Data, &data)
     check(err, "unable to unmarshal stream data")
     fmt.Printf("Received data: event: %s, type: %s, time: %d, img: %s\n", data.Event, data.Type, data.Time, data.Img)
+
+    if data.Event == "heartbeat" || data.Type == "" || data.Img == "" {
+      continue
+    }
+    message := fmt.Sprintf("Pepito is %s: %s", data.Type, data.Img)
+    payload := webhookPayload{ Content: message, AvatarURL: config.AvatarUrl, Username: "Pepito-Bot"}
+    bts, err := json.Marshal(payload)
+    check(err, "can't marshal payload")
+
+    req, err := http.NewRequest("POST", config.WebUrl, bytes.NewReader(bts))
+    check(err, "")
+    req.Header.Set("Content-Type", "application/json")
+
+    resp, err := client.Do(req)
+    check(err, "")
+
+    bd, err := io.ReadAll(resp.Body)
+    check(err, "")
+    fmt.Printf("%s\n", string(bd))
   }
 }
-
 func check(err error, msg string) {
   if err != nil {
     fmt.Fprintf(os.Stderr, msg)
